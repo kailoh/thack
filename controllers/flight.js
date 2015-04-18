2f21987b0af5833de4a9b16e0cc6853fd8bca5a1
Kai <kailoh@users.noreply.github.com>
2015-04-18 16:53:45 -0700

    change log

--- controllers/flight.js
+++ controllers/flight.js
@@ -3,7 +3,7 @@ var flight = require('../models/Flight.js');
 exports.getFlights = function(req, res) {
   flight.Flight.find(function(err, docs) {
     res.render('flights', { flights: docs });
-    console.log(docs);
+    console.log("GET FLIGHTS");
   });
 };
 
