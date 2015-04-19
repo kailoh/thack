b512448a057ab3645df5fef5ac13767e7f59e89b
Kai <kailoh@users.noreply.github.com>
2015-04-18 21:19:59 -0700

    afsafa

--- app.js
+++ app.js
@@ -1,54 +1,54 @@
 /**
  * Module dependencies.
  */
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var compress = require('compression');
-var session = require('express-session');
-var bodyParser = require('body-parser');
-var logger = require('morgan');
-var errorHandler = require('errorhandler');
-var methodOverride = require('method-override');
-var multer  = require('multer');
+ var express = require('express');
+ var cookieParser = require('cookie-parser');
+ var compress = require('compression');
+ var session = require('express-session');
+ var bodyParser = require('body-parser');
+ var logger = require('morgan');
+ var errorHandler = require('errorhandler');
+ var methodOverride = require('method-override');
+ var multer  = require('multer');
 
-var _ = require('lodash');
-var MongoStore = require('connect-mongo')(session);
-var flash = require('express-flash');
-var path = require('path');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var expressValidator = require('express-validator');
-var connectAssets = require('connect-assets');
+ var _ = require('lodash');
+ var MongoStore = require('connect-mongo')(session);
+ var flash = require('express-flash');
+ var path = require('path');
+ var mongoose = require('mongoose');
+ var passport = require('passport');
+ var expressValidator = require('express-validator');
+ var connectAssets = require('connect-assets');
 
-var seeder = require('./seeder');
+ var seeder = require('./seeder');
 
 /**
  * Controllers (route handlers).
  */
-var homeController = require('./controllers/home');
-var userController = require('./controllers/user');
-var apiController = require('./controllers/api');
-var flightController = require('./controllers/flight');
+ var homeController = require('./controllers/home');
+ var userController = require('./controllers/user');
+ var apiController = require('./controllers/api');
+ var flightController = require('./controllers/flight');
 
 /**
  * API keys and Passport configuration.
  */
-var secrets = require('./config/secrets');
-var passportConf = require('./config/passport');
+ var secrets = require('./config/secrets');
+ var passportConf = require('./config/passport');
 
 /**
  * Create Express server.
  */
-var app = express();
+ var app = express();
 
 /**
  * Connect to MongoDB.
  */
-mongoose.connect(secrets.db);
-mongoose.connection.on('error', function() {
+ mongoose.connect(secrets.db);
+ mongoose.connection.on('error', function() {
   console.error('MongoDB Connection Error. Please make sure that MongoDB is running.');
 });
-mongoose.connection.on('open', function() {
+ mongoose.connection.on('open', function() {
   console.log('Connected to db...');
   console.log('environment: ' + app.get('env'));
   if (app.get('env') == 'development'){
@@ -59,100 +59,129 @@ mongoose.connection.on('open', function() {
 /**
  * Express configuration.
  */
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(compress());
-app.use(connectAssets({
+ app.set('port', process.env.PORT || 3000);
+ app.set('views', path.join(__dirname, 'views'));
+ app.set('view engine', 'jade');
+ app.use(compress());
+ app.use(connectAssets({
   paths: [path.join(__dirname, 'public/css'), path.join(__dirname, 'public/js')]
 }));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+ app.use(logger('dev'));
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({ extended: true }));
+
+
 app.use(multer({ dest: path.join(__dirname, 'uploads'),
 
-onFileUploadComplete: function (file, req, res) {
-  console.log(file.fieldname + ' uploaded to  ' + file.path);
+  onFileUploadComplete: function (file, req, res) {
+    console.log(file.fieldname + ' uploaded to  ' + file.path);
 
-  fs = require('fs')
-  fs.readFile(file.path, 'utf8', function (err,data) {
-    if (err) {
-      return console.log(err);
-    }
-    //onsole.log(data);
+    fs = require('fs')
+    fs.readFile(file.path, 'utf8', function (err,data) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(data);
+      
+      var origin;
+      var destination;
+      var distance;
+      var price;
+      var carbon;
+      var departureDate;
+      var arrivalDate;
 
-    var airportCode = data.match(/<airport-code>(.*?)<\/airport-code>/g).map(function(val){
-   console.log(val.replace(/<\/?airport-code>/g,''));
-  });
+      data.match(/<airport-code>(.*?)<\/airport-code>/g).map(function(val){
+        var airportCodeArray = val.replace(/<\/?airport-code>/g,'');
+        origin = airportCodeArray[0];
+        destination = airportCodeArray[1];
+      });
 
-  });
+      data.match(/<duration>(.*?)<\/duration>/g).map(function(val){
+        var durationArray = val.replace(/<\/?duration>/g,'');
+        duration = durationArray[0];
+      });
 
-}
+      data.match(/<utc-date-time>(.*?)<\/utc-date-time>/g).map(function(val){
+        var dateTimeArray = val.replace(/<\/?utc-date-time>/g,'');
+        departureDate = dateTimeArray[0];
+        arrivalDate = dateTimeArray[1];
+      });
 
- }));
-app.use(expressValidator());
-app.use(methodOverride());
-app.use(cookieParser());
-app.use(session({
+      data.match(/<distance-in-miles>(.*?)<\/distance-in-miles>/g).map(function(val){
+        var distanceArray = val.replace(/<\/?distance-in-miles>/g,'');
+        distance = distanceArray[0];
+      });
+
+
+    });
+  }
+}));
+
+
+ app.use(expressValidator());
+ app.use(methodOverride());
+ app.use(cookieParser());
+ app.use(session({
   resave: true,
   saveUninitialized: true,
   secret: secrets.sessionSecret,
   store: new MongoStore({ url: secrets.db, autoReconnect: true })
 }));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
-app.use(function(req, res, next) {
+ app.use(passport.initialize());
+ app.use(passport.session());
+ app.use(flash());
+ app.use(function(req, res, next) {
   res.locals.user = req.user;
   next();
 });
-app.use(function(req, res, next) {
+ app.use(function(req, res, next) {
   if (/api/i.test(req.path)) req.session.returnTo = req.path;
   next();
 });
-app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
+ app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
 
 /**
  * Primary app routes.
  */
-app.get('/', homeController.index);
-app.get('/login', userController.getLogin);
-app.post('/login', userController.postLogin);
-app.get('/logout', userController.logout);
-app.get('/forgot', userController.getForgot);
-app.post('/forgot', userController.postForgot);
-app.get('/reset/:token', userController.getReset);
-app.post('/reset/:token', userController.postReset);
-app.get('/signup', userController.getSignup);
-app.post('/signup', userController.postSignup);
-app.get('/account', passportConf.isAuthenticated, userController.getAccount);
-app.post('/account/profile', passportConf.isAuthenticated, userController.postUpdateProfile);
-app.post('/account/password', passportConf.isAuthenticated, userController.postUpdatePassword);
-app.post('/account/delete', passportConf.isAuthenticated, userController.postDeleteAccount);
-app.get('/account/unlink/:provider', passportConf.isAuthenticated, userController.getOauthUnlink);
+ app.get('/', homeController.index);
+ app.get('/login', userController.getLogin);
+ app.post('/login', userController.postLogin);
+ app.get('/logout', userController.logout);
+ app.get('/forgot', userController.getForgot);
+ app.post('/forgot', userController.postForgot);
+ app.get('/reset/:token', userController.getReset);
+ app.post('/reset/:token', userController.postReset);
+ app.get('/signup', userController.getSignup);
+ app.post('/signup', userController.postSignup);
+ app.get('/account', passportConf.isAuthenticated, userController.getAccount);
+ app.post('/account/profile', passportConf.isAuthenticated, userController.postUpdateProfile);
+ app.post('/account/password', passportConf.isAuthenticated, userController.postUpdatePassword);
+ app.post('/account/delete', passportConf.isAuthenticated, userController.postDeleteAccount);
+ app.get('/account/unlink/:provider', passportConf.isAuthenticated, userController.getOauthUnlink);
 
 /**
  * API examples routes.
  */
-app.get('/api', apiController.getApi);
-app.get('/api/facebook', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.getFacebook);
-app.get('/api/twitter', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.getTwitter);
-app.post('/api/twitter', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.postTwitter);
+ app.get('/api', apiController.getApi);
+ app.get('/api/facebook', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.getFacebook);
+ app.get('/api/twitter', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.getTwitter);
+ app.post('/api/twitter', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.postTwitter);
 
 /**
  * OAuth authentication routes. (Sign in)
  */
 
-app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'user_location'] }));
-app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), function(req, res) {
+ app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'user_location'] }));
+ app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), function(req, res) {
   res.redirect(req.session.returnTo || '/');
 });
-app.get('/auth/google', passport.authenticate('google', { scope: 'profile email' }));
-app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), function(req, res) {
+ app.get('/auth/google', passport.authenticate('google', { scope: 'profile email' }));
+ app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), function(req, res) {
   res.redirect(req.session.returnTo || '/');
 });
-app.get('/auth/twitter', passport.authenticate('twitter'));
-app.get('/auth/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/login' }), function(req, res) {
+ app.get('/auth/twitter', passport.authenticate('twitter'));
+ app.get('/auth/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/login' }), function(req, res) {
   res.redirect(req.session.returnTo || '/');
 });
 
@@ -165,13 +194,13 @@ app.post('/flights', flightController.postFlight);
 /**
  * Error Handler.
  */
-app.use(errorHandler());
+ app.use(errorHandler());
 
 /**
  * Start Express server.
  */
-app.listen(app.get('port'), function() {
+ app.listen(app.get('port'), function() {
   console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
 });
 
-module.exports = app;
+ module.exports = app;
