67308d246224fe9fcf2a7fb795b8982c831871a8
Kai <kailoh@users.noreply.github.com>
2015-04-18 20:35:48 -0700

    post config

--- controllers/flight.js
+++ controllers/flight.js
@@ -1,20 +1,37 @@
 var flight = require('../models/Flight.js');
 
+var http = require('http'),
+    inspect = require('util').inspect;
+
+var Busboy = require('busboy');
+
 exports.getFlights = function(req, res) {
-  flight.Flight.find(function(err, docs) {
-    res.render('flights', { flights: docs });
-    console.log("GET FLIGHTS");
-  });
+	flight.Flight.find(function(err, docs) {
+		res.render('flights', { flights: docs });
+		console.log("GET FLIGHTS");
+	});
 };
 
 exports.postFlight = function(req, res, next) {
-  console.log("POST FLIGHTS RECEIVED");
-  console.log("POST ALL: " + req);
-  console.log(req);
-  console.log("POST BODY percent: %j", req.body);
-  console.log("POST BODY: " + req.body);
-  console.log("POST PARAMS: " + req.params);
-  console.log("POST QUERY: " + req.query);
-  console.log("POST HEADER: " + req.headers);
-  res.end('Thanks for submitting a flight. It worked!');
+
+	var busboy = new Busboy({ headers: req.headers });
+    busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+      console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
+      file.on('data', function(data) {
+        console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
+      });
+      file.on('end', function() {
+        console.log('File [' + fieldname + '] Finished');
+      });
+    });
+    busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated) {
+      console.log('Field [' + fieldname + ']: value: ' + inspect(val));
+    });
+    busboy.on('finish', function() {
+      console.log('Done parsing form!');
+      res.writeHead(303, { Connection: 'close', Location: '/' });
+      res.end();
+    });
+    req.pipe(busboy);
 };
+
